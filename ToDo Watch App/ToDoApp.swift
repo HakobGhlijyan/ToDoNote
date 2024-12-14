fe8f7512dcffbcad6637050dba17623d50a90f47
Ghlijyan Hakob <hakobghlijyan@gmail.com>
2024-12-14 15:49:08 +0400

    Initial Commit

---
+++ ToDo Watch App/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo Watch App
+//
+//  Created by Hakob Ghlijyan on 12/14/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDo_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
